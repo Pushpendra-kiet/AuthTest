1b9f119d40f9701f5c05e79262334532a2245c0a
programminghunk <pshpndr.kumar@gmail.com>
2025-05-22 14:41:57 +0530

    fg
    gdfg

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ const client = new OAuth2Client(
   process.env.GOOGLE_REDIRECT_URI
 );
 
-// Middleware for handling sessions
-// app.use(
-//   session({
-//     secret: crypto.randomBytes(64).toString('hex'),
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
+//Middleware for handling sessions
+app.use(
+  session({
+    secret: process.env.AUTH_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
